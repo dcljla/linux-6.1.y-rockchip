826b67565950ba80ba1b773f4b411c9e74a0706a
uniqfreq <flippy@sina.com>
2026-01-29 09:58:28 +0800

    drv: net: wireless: the aic8800_usb firmware load path can be obtained from Kconfig.

--- drivers/net/wireless/aicsemi/aic8800/usb/bsp/aicbluetooth.c
+++ drivers/net/wireless/aicsemi/aic8800/usb/bsp/aicbluetooth.c
@@ -175,10 +175,14 @@ enum aicbsp_cpmode_type {
 #define AIC_HW_INFO 0x21
 
 #define FW_PATH_MAX 200
+#if defined(CONFIG_AIC_FW_PATH)
+static const char* aic_default_fw_path = CONFIG_AIC_FW_PATH;
+#else
 #if defined(CONFIG_PLATFORM_UBUNTU)
 static const char* aic_default_fw_path = "/lib/firmware";
 #else
 static const char* aic_default_fw_path = "/vendor/etc/firmware";
+#endif
 #endif
 char aic_fw_path[FW_PATH_MAX];
 module_param_string(aic_fw_path, aic_fw_path, FW_PATH_MAX, 0660);
